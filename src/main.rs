168058922aef5ee50ed22cefae75a7c5485b391f
Rasengangstarr <davidouthere@msn.com>
2020-08-03 17:36:30 +0100

    finito

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use std::env;
 use std::fs::File;
 use std::io::{self, prelude::*, BufReader};
-
+use std::{thread, time};
 
 fn main() {
     println!("Hello, world!");
@@ -20,52 +20,59 @@ fn main() {
 
         for (x, row) in state.iter().enumerate() {
             for (y, _col) in row.iter().enumerate() {
-                if x < 1 || y < 1 || x > 9 || y > 9 {
+                if x < 1 || y < 1 || x > 8 || y > 8 {
                     continue;
                 }
+                //println!("x{},y{}",x,y);
                 let xi = x as i8;
                 let yi = y as i8;
 
                 let mut tot = 0;
 
-                let i : i8 = xi-1;
-                let j : i8 = yi-1;
+                let mut i : i8 = xi-1;
+                let mut j : i8 = yi-1;
 
                 while i <= xi+1 {
                     while j <= yi+1 {
-                        if i != 0 || j != 0 {
-                            println!("{},{}",i,j);
-                            let ii : usize = i as usize;
-                            let ji : usize = i as usize;
+                        if i != xi || j != yi {
                             
+                            let ii : usize = i as usize;
+                            let ji : usize = j as usize;
+
                             if state[ii][ji] {
                                 tot += 1;
                             }
                         }
+                        j += 1;
                     }
+                    j = yi-1;
+                    i += 1;
                 }
                 buff[x][y] = (state[x][y] && tot == 2) || tot == 3;
+                //println!("{}{}{}", x, y, tot);
             }
         }
-
-        println!("loop");
 
         for (x, row) in buff.iter().enumerate() {
             for (y, _col) in row.iter().enumerate() {
                 let chr = if buff[x][y] {"X"} else {"0"};
-                print!("{}", chr)
+                print!("{}", chr);
+                state[x][y] = buff[x][y];
             }
             print!("\n");
         }
-        println!("loop");
+        let ten_millis = time::Duration::from_millis(1000);
+        thread::sleep(ten_millis);
+        print!("{}[2J", 27 as char);
+
+        //Clear buffer
+        for (x, row) in state.iter().enumerate() {
+            for (y, _col) in row.iter().enumerate() {
+                buff[x][y] = false;
+            }
+        }
         
-        
-        state = buff;
-        println!("loop");
     }
-    println!("loop");
-
-
 }
 
 fn read_file(filename : String) -> io::Result<[[bool; 10]; 10]> {
